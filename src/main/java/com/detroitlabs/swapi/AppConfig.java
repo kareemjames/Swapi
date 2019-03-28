66c5b56f8b4305934b0acf85b97e006298783c71
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-28 19:38:05 -0400

    add app config

--- src/main/java/com/detroitlabs/swapi/AppConfig.java
+++ src/main/java/com/detroitlabs/swapi/AppConfig.java
@@ -1,5 +1,12 @@
 package com.detroitlabs.swapi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
 }
