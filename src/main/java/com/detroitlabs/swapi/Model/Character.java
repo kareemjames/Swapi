3a192be631d1dcd733241684b0a9f6650dd2b8f4
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-29 16:47:12 -0400

    create chracter model

--- src/main/java/com/detroitlabs/swapi/Model/Character.java
+++ src/main/java/com/detroitlabs/swapi/Model/Character.java
@@ -1,4 +1,57 @@
 package com.detroitlabs.swapi.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Character {
+
+    private String name;
+    private String birthYear;
+    private String gender;
+    private Homeworld homeworld;
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", birthYear='" + birthYear + '\'' +
+                ", gender='" + gender + '\'' +
+                ", homeworld=" + homeworld +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("birth_year")
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    @JsonProperty("birth_year")
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Homeworld getHomeworld() {
+        return homeworld;
+    }
+
+    public void setHomeworld(Homeworld homeworld) {
+        this.homeworld = homeworld;
+    }
 }
