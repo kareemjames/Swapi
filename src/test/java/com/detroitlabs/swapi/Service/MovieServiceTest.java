d838e0c3bde6dbcc1976669a3134d0856fe841a2
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-04-04 21:31:09 -0400

    add tests for all api calls, tested successfully

--- src/test/java/com/detroitlabs/swapi/Service/MovieServiceTest.java
+++ src/test/java/com/detroitlabs/swapi/Service/MovieServiceTest.java
@@ -1,6 +1,8 @@
 package com.detroitlabs.swapi.Service;
 
+import com.detroitlabs.swapi.Model.HomeWorld;
 import com.detroitlabs.swapi.Model.Movie;
+import com.detroitlabs.swapi.Model.Character;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,17 +21,27 @@ public class MovieServiceTest {
     @Test
     public void fetchMovie() {
         Movie movie = movieService.fetchMovie();
-        String expectedReult = "The Empire Strikes Back";
+        String expectedResult = "The Empire Strikes Back";
         String resultOfMethodCall = movie.getTitle();
 
-        assertEquals(expectedReult, resultOfMethodCall);
+        assertEquals(expectedResult, resultOfMethodCall);
     }
 
     @Test
     public void fetchCharacter() {
+        Character character = movieService.fetchCharacter("https://swapi.co/api/people/1/");
+        String expectedResult = "Luke Skywalker";
+        String resultOfMethodCall = character.getName();
+
+        assertEquals(expectedResult, resultOfMethodCall);
     }
 
     @Test
     public void fetchHomeWorld() {
+        HomeWorld homeWorld = movieService.fetchHomeWorld("https://swapi.co/api/planets/1/");
+        String expectedResult = "Tatooine";
+        String resultOfMethodCall = homeWorld.getName();
+
+        assertEquals(expectedResult, resultOfMethodCall);
     }
 }
